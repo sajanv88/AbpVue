c4078fe9d656a567867b17e4fab8dabad52771b1
sajanv88 <work@sajankumarv.com>
2024-02-24 11:38:56 +0100

    fix:(routing): Fixed based routing path

--- components/shared/DefaultNav.vue
+++ components/shared/DefaultNav.vue
@@ -56,11 +56,15 @@ console.log(currentUser?.isAuthenticated, "currentUser");
                 }}</a
               >
             </li>
-            <li v-show="!currentUser?.isAuthenticated">
+            <li>
               <a
-                href="/api/auth/signup"
+                :href="
+                  currentUser?.isAuthenticated
+                    ? '/api/auth/signout'
+                    : '/api/auth/signup'
+                "
                 class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >Register</a
+                >{{ currentUser?.isAuthenticated ? "Logout" : "Register" }}</a
               >
             </li>
           </ul>
--- middleware/router.global.ts
+++ middleware/router.global.ts
@@ -11,6 +11,9 @@ let validPaths = navList.map(navCallback).filter(Boolean).flat(2);
 validPaths = [...validPaths, "/admin/profile", "/error/notfound"];
 
 export default defineNuxtRouteMiddleware((to, from) => {
+  if (to.fullPath === "/") {
+    return;
+  }
   if (!validPaths.includes(to.fullPath)) {
     return navigateTo("/error/notfound");
   }
