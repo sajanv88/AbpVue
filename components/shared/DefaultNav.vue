b5e750228f32b2c4a9d2e73f26520489f28c8d7a
sajanv88 <work@sajankumarv.com>
2024-03-17 13:53:53 +0100

    fix(mobile-nav): Fixed mobile navigation toggle in default home page

--- components/shared/DefaultNav.vue
+++ components/shared/DefaultNav.vue
@@ -3,8 +3,15 @@ import Icon from "~/components/shared/Icon.vue";
 import { useAbpConfiguration } from "~/store/state";
 import Alert from "~/components/shared/Alert.vue";
 import ThemeSwitch from "~/components/shared/ThemeSwitch.vue";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/abp/ui/dropdown-menu";
 const abp = useAbpConfiguration();
 const currentUser = abp.config?.currentUser;
+
+const toggleNav = ref(false);
 </script>
 
 <template>
@@ -25,21 +32,56 @@ const currentUser = abp.config?.currentUser;
 
         <div class="flex md:order-2 space-x-3 md:ml-10 rtl:space-x-reverse">
           <ThemeSwitch />
-
-          <button
-            data-collapse-toggle="navbar-sticky"
-            type="button"
-            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-sticky"
-            aria-expanded="false"
-          >
-            <span class="sr-only">Open main menu</span>
-            <Icon icon="menu" />
-          </button>
+          <div class="md:hidden">
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Button
+                  type="button"
+                  aria-controls="navbar-sticky"
+                  aria-expanded="false"
+                  variant="ghost"
+                  size="icon"
+                  @click="toggleNav = !toggleNav"
+                >
+                  <span class="sr-only">Open main menu</span>
+                  <Icon icon="menu" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+                  <a
+                    :href="
+                      currentUser?.isAuthenticated
+                        ? '/admin'
+                        : '/api/auth/signin'
+                    "
+                    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    >{{
+                      currentUser?.isAuthenticated
+                        ? currentUser?.userName
+                        : "Login"
+                    }}</a
+                  >
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <a
+                    :href="
+                      currentUser?.isAuthenticated
+                        ? '/api/auth/signout'
+                        : '/api/auth/signup'
+                    "
+                    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    >{{
+                      currentUser?.isAuthenticated ? "Logout" : "Register"
+                    }}</a
+                  >
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         </div>
         <div
-          class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-          id="navbar-sticky"
+          class="items-center justify-between w-full md:flex md:w-auto md:order-1 hidden md:block"
         >
           <ul
             class="flex flex-col p-4 md:p-0 mt-4 space-y-2 md:space-y-0 font-medium border border-gray-100 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:border-gray-700"
